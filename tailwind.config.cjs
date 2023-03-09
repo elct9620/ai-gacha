3dd7afe8d9af3e1ffac8423f44fd395d71689c10
蒼時弦也 <elct9620@frost.tw>
2023-03-09 17:27:37 +0800

    Adjust UI display

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,9 @@ module.exports = {
     extend: {
       fontFamily: {
         'cubic11': ['Cubic11', 'sans-serif']
+      },
+      transitionTimingFunction: {
+        'in-out-back': 'cubic-bezier(0.68, -0.6, 0.32, 1.6)',
       }
     },
   },
