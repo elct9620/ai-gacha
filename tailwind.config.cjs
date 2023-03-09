23b3fea299c502d3a7a9cf07461552d5f5989373
蒼時弦也 <elct9620@frost.tw>
2023-03-09 15:39:17 +0800

    Add Cubic11 font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'cubic11': ['Cubic11', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
