3bf143b30c38c3fe0e1c5963534e23fbaf279581
蒼時弦也 <elct9620@frost.tw>
2023-03-11 19:25:37 +0800

    Add attributes to card

--- src/entities/card.ts
+++ src/entities/card.ts
@@ -1,13 +1,62 @@
+import {
+  Gender, GenderPrompt,
+  HairLength, HairLengthPrompt,
+  HairColor, HairColorPrompt,
+  EyeColor, EyeColorPrompt,
+  Race, RacePrompt,
+} from './prompt'
+
+const BASE_PROMPT = [
+  'mksks style',  'masterpiece',  'best quality',  'ultra-detailed',  'illustration', 'portrait',
+]
+
+const NEGATIVE_PROMPT = [
+  'lowres', '((bad anatomy))', '((bad hands))', 'text', 'missing finger', 'extra digits', 'fewer digits',
+  'blurry', '((mutated hands and fingers))', '(poorly drawn face)', '((mutation))', '((deformed face))',
+  '(ugly)', '((bad proportions))', '((extra limbs))', 'extra face', '(double head)', '(extra head)', '((extra feet))',
+  'monster', 'logo', 'cropped', 'worst quality', 'low quality', 'normal quality', 'jpeg', 'humpbacked', 'long body',
+  'long neck', '((jpeg artifacts))'
+]
+
+export type CardAttribute = {
+  gender?: Gender
+  hairLength?: HairLength
+  hairColor?: HairColor
+  eyeColor?: EyeColor
+  race?: Race
+}
+
 export class Card {
   static readonly WIDTH = 448
   static readonly HEIGHT = 640
 
+  public gender: Gender
+  public hairLength: HairLength
+  public hairColor: HairColor
+  public eyeColor: EyeColor
+  public race: Race
+
+  constructor(attributes?: CardAttribute) {
+    this.gender = attributes?.gender || Gender.Female
+    this.hairLength = attributes?.hairLength || HairLength.Long
+    this.hairColor = attributes?.hairColor || HairColor.White
+    this.eyeColor = attributes?.eyeColor || EyeColor.Golden
+    this.race = attributes?.race || Race.Angel
+  }
+
   toPrompt(): string {
-    return 'mksks style, masterpiece, best quality, ultra-detailed, illustration, close-up, straight on, face focus, 1girl, white hair, golden eyes, long hair, halo, angel wings, serene expression, looking at viewer'
+    return [
+      ...BASE_PROMPT,
+      ...GenderPrompt[this.gender],
+      ...HairLengthPrompt[this.hairLength],
+      ...HairColorPrompt[this.hairColor],
+      ...EyeColorPrompt[this.eyeColor],
+      ...RacePrompt[this.race],
+    ].join(', ')
   }
 
   toNegativePrompt(): string {
-    return 'lowres, ((bad anatomy)), ((bad hands)), text, missing finger, extra digits, fewer digits, blurry, ((mutated hands and fingers)), (poorly drawn face), ((mutation)), ((deformed face)), (ugly), ((bad proportions)), ((extra limbs)), extra face, (double head), (extra head), ((extra feet)), monster, logo, cropped, worst quality, low quality, normal quality, jpeg, humpbacked, long body, long neck, ((jpeg artifacts))'
+    return NEGATIVE_PROMPT.join(', ')
   }
 
   get isHires() {
---
+++ src/entities/prompt.ts
@@ -0,0 +1,80 @@
+export enum Gender {
+  Male,
+  Female
+}
+
+export const GenderPrompt = {
+  [Gender.Male]: ['1boy'],
+  [Gender.Female]: ['1girl']
+}
+
+export enum HairLength {
+  Short,
+  Medium,
+  Long
+}
+
+export const HairLengthPrompt = {
+  [HairLength.Short]: ['short hair'],
+  [HairLength.Medium]: ['medium hair'],
+  [HairLength.Long]: ['long hair']
+}
+
+export enum HairColor {
+  White,
+  Blue,
+  Cyan,
+  Green,
+  Red,
+  Purple,
+  Golden,
+  Brown
+}
+
+export const HairColorPrompt = {
+  [HairColor.White]: ['white hair'],
+  [HairColor.Blue]: ['blue hair'],
+  [HairColor.Cyan]: ['cyan hair'],
+  [HairColor.Green]: ['green hair'],
+  [HairColor.Red]: ['red hair'],
+  [HairColor.Purple]: ['purple hair'],
+  [HairColor.Golden]: ['golden hair'],
+  [HairColor.Brown]: ['brown hair']
+}
+
+export enum EyeColor {
+  Golden = 'golden eyes',
+  Blue = 'blue eyes',
+  Green = 'green eyes',
+  Red = 'red eyes'
+}
+
+export const EyeColorPrompt = {
+  [EyeColor.Golden]: ['golden eyes'],
+  [EyeColor.Blue]: ['blue eyes'],
+  [EyeColor.Green]: ['green eyes'],
+  [EyeColor.Red]: ['red eyes']
+}
+
+export enum Race {
+  Angel,
+  Demon,
+  Vampire,
+  Elf,
+  Dragon,
+  Human
+}
+
+export const RacePrompt = {
+  [Race.Angel]: ['angel', 'halo', 'wings'],
+  [Race.Demon]: ['demon'],
+  [Race.Vampire]: ['vampire'],
+  [Race.Elf]: ['elf'],
+  [Race.Dragon]: ['dragon'],
+  [Race.Human]: ['human']
+}
+
+export const TraitPrompt = [
+  'indoors', 'dimly lit', 'upper body','close-up', 'straight on', 'face focus', 'looking at viewer',
+  'serene expression', 'chains', 'beam', 'energy' , 'chaos', 'throne room'
+]
